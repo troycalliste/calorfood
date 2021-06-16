e5025b5651f68e12d8acadd9ed305c934349d150
troycalliste <troycallist@gmail.com>
2021-06-16 10:57:44 -0400

    s

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,5 +1,5 @@
 class InquiryMailer < ApplicationMailer
-  def inquiry_notification(name, email, subject, message)
+  def inquiry_notification(name, phone)
 @name = name if name
 @phone = phone if phone
 mail(to: ['troycallist@gmail.com'], subject: 'You have a new inquiry!')
